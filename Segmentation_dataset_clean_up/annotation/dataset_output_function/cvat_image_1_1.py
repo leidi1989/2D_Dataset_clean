7621d3f6d76d942cb20f53571c6f04c658f202ec
leidi1989 <331926283@qq.com>
2021-11-12 18:07:06 +0800

    update

--- Segmentation_dataset_clean_up/annotation/dataset_output_function/cvat_image_1_1.py
+++ Segmentation_dataset_clean_up/annotation/dataset_output_function/cvat_image_1_1.py
@@ -4,7 +4,7 @@ Version:
 Author: Leidi
 Date: 2021-10-13 18:36:09
 LastEditors: Leidi
-LastEditTime: 2021-11-12 18:05:15
+LastEditTime: 2021-11-12 18:06:59
 '''
 import os
 import json
@@ -95,6 +95,7 @@ def annotation_get_temp(dataset: dict, temp_annotation_path: str) -> None:
         dataset (dict): [数据集信息字典]
         temp_annotation_path (str): [暂存标签路径]
     """
+    
     image = TEMP_LOAD(dataset, temp_annotation_path)
     if image == None:
         return
