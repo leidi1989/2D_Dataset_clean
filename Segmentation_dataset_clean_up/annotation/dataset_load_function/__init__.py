dce8cb4c0476ca66205f07e236931d5d024b4b7b
leidi1989 <331926283@qq.com>
2021-12-06 16:17:41 +0800

    update

--- Segmentation_dataset_clean_up/annotation/dataset_load_function/__init__.py
+++ Segmentation_dataset_clean_up/annotation/dataset_load_function/__init__.py
@@ -4,13 +4,13 @@ Version:
 Author: Leidi
 Date: 2021-08-05 15:12:25
 LastEditors: Leidi
-LastEditTime: 2021-11-15 13:48:53
+LastEditTime: 2021-12-06 16:17:33
 '''
 from . import apolloscape_lane_segment
 from . import bdd100k
 from . import cityscapes
 from . import coco2017
 from . import cvat_image_1_1
-from . import huawei_segment
+from . import huaweiyun_segment
 from . import hy_val
 from . import yunce_segment
