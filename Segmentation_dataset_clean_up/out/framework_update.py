d8da767067df55a96b69ca1800feebaa417d4896
leidi1989 <331926283@qq.com>
2021-12-06 16:50:52 +0800

    update

--- Segmentation_dataset_clean_up/out/framework_update.py
+++ Segmentation_dataset_clean_up/out/framework_update.py
@@ -4,7 +4,7 @@ Version:
 Author: Leidi
 Date: 2021-08-11 03:28:09
 LastEditors: Leidi
-LastEditTime: 2021-12-06 16:49:10
+LastEditTime: 2021-12-06 16:50:43
 '''
 import os
 import cv2
@@ -245,3 +245,8 @@ def cityscapes_val(dataset: dict) -> None:
         cv2.imwrite(labelIds_output_path, zeros)
 
     return
+
+
+def cvat_image_1_1(dataset: dict) -> None:
+    
+    pass
