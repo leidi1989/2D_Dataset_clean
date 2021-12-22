012550e902873d33167acd593182f0a930917b8f
leidi1989 <331926283@qq.com>
2021-12-22 18:33:06 +0800

    update

--- Tool/tool_ros_to_img.py
+++ Tool/tool_ros_to_img.py
@@ -2,97 +2,80 @@
 Description: 
 Version: 
 Author: Leidi
-Date: 2021-12-08 16:34:03
+Date: 2021-12-22 18:21:36
 LastEditors: Leidi
-LastEditTime: 2021-12-22 18:20:01
+LastEditTime: 2021-12-22 18:30:14
 '''
-import cv2
-import rospy
+import os
+import os.path
+import re
+import cv_bridge
+import roslib
 import rosbag
-import argparse
+import rospy
+import cv2
+import time
+from sensor_msgs.msg import Image
 from cv_bridge import CvBridge
 from cv_bridge import CvBridgeError
 
 
-# bag_file = r'/mnt/data_1/Data/rosbag/qunguang/20211220/_2021-12-20-16-50-26_0.bag'
-# bag = rosbag.Bag(bag_file, 'r')
-
-# bridge = CvBridge()
-# bag_data = bag.read_messages('/cam_front_center/csi_cam/image_raw/compressed')
-# for topic, msg, t in bag_data:
-#     cv_image = bridge.compressed_imgmsg_to_cv2(msg, "bgr8")
-#     cv2.imshow("Image window", cv_image)
-#     cv2.waitKey(0)
-
-image_output_path = r'/mnt/data_1/Data/rosbag/images/qunguang_2021-12-07-11-47-22'
-bag_path = r'/mnt/data_1/Data/rosbag/qunguang/20211209/1209_qunguang1_2_2021-12-09-11-01-17_1.bag'
-
-
+def bool_topic(topic):
+    if(topic == "/cam_left_back/csi_cam/image_raw/compressed"):
+        return 0
+    elif (topic == "/cam_right_back/csi_cam/image_raw/compressed"):
+        return 0
+    elif (topic == "/cam_right_front/csi_cam/image_raw/compressed"):
+        return 0
+    elif (topic == "/cam_left_front/csi_cam/image_raw/compressed"):
+        return 0
+    elif (topic == "/cam_front_center/csi_cam/image_raw/compressed"):
+        return 1
+    elif (topic == "/cam_back/csi_cam/image_raw/compressed"):
+        return 2
+    elif (topic == "/cam_right_fish/csi_cam/image_raw/compressed"):
+        return 0
+    elif (topic == "/cam_front_fish/csi_cam/image_raw/compressed"):
+        return 0
+    elif (topic == "/cam_back_fish/csi_cam/image_raw/compressed"):
+        return 0
+    elif (topic == "/cam_left_fish/csi_cam/image_raw/compressed"):
+        return 0
+    else : 
+        return 0
+        
 class ImageCreate():
-    def __init__(self):
+    def __init__(self,filename):
         self.bridge = CvBridge()
-        i = 0
-        num = 0
-        with rosbag.Bag(bag_path, 'r') as bag:
-            for topic, msg, t in bag.read_messages():
-                if topic == '/cam_front_center/csi_cam/image_raw/compressed':
-                    i += 1
-                    if i % 3 != 0:
+        topic_LIST = [0,0,0,0,0,0,0,0,0,0,0]
+        print(filename)
+        with rosbag.Bag(filename,'r') as bag:
+            for topic,msg,t in bag.read_messages():
+                if bool_topic(topic) >= 1:
+                    cv_path = path + topic.split('/')[1]
+                    topic_LIST[bool_topic(topic)] += 1
+                    if topic_LIST[bool_topic(topic)] % num_count != 0 :
                         continue
                     try:
-                        cv_image = self.bridge.compressed_imgmsg_to_cv2(
-                            msg, 'bgr8')
+                        cv_image = self.bridge.compressed_imgmsg_to_cv2(msg,"bgr8")
                     except CvBridgeError as e:
-                        print(e)
-                    timestr = '%.6f' % msg.header.stamp.to_sec()
-                    image_name = timestr+'.png'
-                    cv2.imwrite(image_output_path +
-                                'cam_left_front/'+image_name, cv_image)
-                if topic == '/cam_front_center/csi_cam/image_raw/compressed':
-                    num += 1
-                    if num % 3 != 0:
-                        continue
-                    try:
-                        cv_image = self.bridge.compressed_imgmsg_to_cv2(
-                            msg, 'bgr8')
-                    except CvBridgeError as e:
-                        print(e)
-                    timestr = '%.6f' % msg.header.stamp.to_sec()
-                    image_name = timestr+'.png'
-                    cv2.imwrite(image_output_path +
-                                'cam_front_center/'+image_name, cv_image)
-                if topic == '/cam_right_front/csi_cam/image_raw/compressed':
-                    num += 1
-                    if num % 3 != 0:
-                        continue
-                    try:
-                        cv_image = self.bridge.compressed_imgmsg_to_cv2(
-                            msg, 'bgr8')
-                    except CvBridgeError as e:
-                        print(e)
-                    timestr = '%.6f' % msg.header.stamp.to_sec()
-                    image_name = timestr+'.png'
-                    cv2.imwrite(image_output_path +
-                                'cam_front_center/'+image_name, cv_image)
-
-
-if __name__ == "__main__":
-    parser = argparse.ArgumentParser(prog='tool_avi_to_mp4.py')
-    parser.add_argument('--avipath', default=r'',
-                        type=str, help='avi path')
-    parser.add_argument('--imgpath', default=r'/mnt/data_1/Dataset/detect_output/YOLOP/20211209/cam_front_center_20210918_output_20211209',
-                        type=str, help='image output path')
-    parser.add_argument('--pref', default=r'',
-                        type=str, help='rename prefix')
-    parser.add_argument('--mode', default='',
-                        type=str, help='image output')
-    parser.add_argument('--time', default=1,
-                        type=int, help='the time of create image, secend')
-    parser.add_argument('--mp4fps', default=30,
-                        type=int, help='the fps of concate images.')
-    opt = parser.parse_args()
-
-    try:
-        image_creator = ImageCreate()
-    except rospy.ROSInterruptException as e:
-        print(e)
+                        print (e)
+                    timestr = "%.6f" % msg.header.stamp.to_sec()
+                    image_name = timestr+".png"
+                    if not os.path.exists(cv_path):
+                        os.makedirs(cv_path)
+                    cv2.imwrite(cv_path+'/'+image_name,cv_image)
+                
+    
+if __name__ == '__main__':
+    path=r'/mnt/data_1/Data/rosbag/images/qunguang_2021-12-07-11-47-22'
+    path2 =  r'/mnt/data_1/Data/rosbag/qunguang/20211209/'
+    num_count = 15
+    for filename in os.listdir(path2):
+        paron = r"1209_qunguang1_2_2021-12-09-11-01-17_1.bag"
+        if re.search(paron,filename) != None:
+            print(os.path.join(path2, filename))
+            try:
+                image_creator = ImageCreate(os.path.join(path2, filename))
+            except rospy.ROSInterruptException:
+                pass
