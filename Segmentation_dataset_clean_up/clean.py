89e0b3f48488331f338d6de060fa77f10fb4d62f
leidi1989 <331926283@qq.com>
2021-12-22 11:30:38 +0800

    update

--- Segmentation_dataset_clean_up/clean.py
+++ Segmentation_dataset_clean_up/clean.py
@@ -4,7 +4,7 @@ Version:
 Author: Leidi
 Date: 2021-08-04 16:45:50
 LastEditors: Leidi
-LastEditTime: 2021-12-22 09:39:31
+LastEditTime: 2021-12-22 11:30:33
 '''
 from utils.utils import *
 from input import source_dataset
@@ -28,15 +28,15 @@ def main(dataset_info: dict) -> None:
         dataset_info (dict): [数据集信息字典]
     """
 
-    # print('\nStart copy images and annotations:')
-    # source_dataset.__dict__[dataset_info['source_dataset_stype']](dataset_info)
+    print('\nStart copy images and annotations:')
+    source_dataset.__dict__[dataset_info['source_dataset_stype']](dataset_info)
 
-    # print('\nStart to transform source annotation to temp annotation:')
-    # annotation_load.__dict__[
-    #     dataset_info['source_dataset_stype']](dataset_info)
+    print('\nStart to transform source annotation to temp annotation:')
+    annotation_load.__dict__[
+        dataset_info['source_dataset_stype']](dataset_info)
 
-    # print('\nStart to analyze dataset:')
-    # information(dataset_info)
+    print('\nStart to analyze dataset:')
+    information(dataset_info)
 
     print('\nStart output temp dataset annotations to target annotations:')
     dataset_info['temp_annotation_path_list'] = temp_annotation_path_list(
