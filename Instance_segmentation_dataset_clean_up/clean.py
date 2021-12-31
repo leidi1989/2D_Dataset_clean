320d921942554df45fbf4507b27440c3b9355b0c
leidi1989 <331926283@qq.com>
2021-12-31 16:42:30 +0800

    update

--- Instance_segmentation_dataset_clean_up/clean.py
+++ Instance_segmentation_dataset_clean_up/clean.py
@@ -4,7 +4,7 @@ Version:
 Author: Leidi
 Date: 2021-08-04 16:45:50
 LastEditors: Leidi
-LastEditTime: 2021-12-31 16:19:31
+LastEditTime: 2021-12-31 16:38:22
 '''
 import os
 import time
@@ -29,15 +29,15 @@ def main(dataset_info: dict) -> None:
         dataset_info (dict): [数据集信息字典]
     """
 
-    # print('\nStart copy images and annotations:')
-    # source_dataset.__dict__[dataset_info['source_dataset_stype']](dataset_info)
+    print('\nStart copy images and annotations:')
+    source_dataset.__dict__[dataset_info['source_dataset_stype']](dataset_info)
 
-    # print('\nStart to transform source annotation to temp annotation:')
-    # annotation_load.__dict__[
-    #     dataset_info['source_dataset_stype']](dataset_info)
+    print('\nStart to transform source annotation to temp annotation:')
+    annotation_load.__dict__[
+        dataset_info['source_dataset_stype']](dataset_info)
 
-    # print('\nStart to analyze dataset:')
-    # information(dataset_info)
+    print('\nStart to analyze dataset:')
+    information(dataset_info)
 
     print('\nStart output temp dataset annotations to target annotations:')
     dataset_info['temp_annotation_path_list'] = temp_annotation_path_list(
@@ -48,9 +48,9 @@ def main(dataset_info: dict) -> None:
     print('\nStart check target annotations:')
     check(dataset_info)
 
-    # print('\nStart update framework:')
-    # framework_update.__dict__[
-    #     dataset_info['target_dataset_style']](dataset_info)
+    print('\nStart update framework:')
+    framework_update.__dict__[
+        dataset_info['target_dataset_style']](dataset_info)
 
     return
 
