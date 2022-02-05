cc11c3959a83f8141c3a4254bef542fa8dd1b08b
leidi1989 <331926283@qq.com>
2022-02-06 00:52:49 +0800

    update

--- Clean_up/dataset/dataset_characteristic.py
+++ Clean_up/dataset/dataset_characteristic.py
@@ -4,53 +4,54 @@ Version:
 Author: Leidi
 Date: 2021-08-09 21:55:18
 LastEditors: Leidi
-LastEditTime: 2022-02-06 00:40:56
+LastEditTime: 2022-02-06 00:52:03
 '''
 
 # 输入数据集图片、annotation文件格式
-TARGET_DATASET_FILE_FORM = {'apolloscape_lane_segment': {'image': 'jpg',
-                                                  'annotation': 'png'
-                                                  },
-                     'bdd100k': {'image': 'jpg',
-                                 'annotation': 'json'
+TARGET_DATASET_FILE_FORM = {
+    'apolloscape_lane_segment': {'image': 'jpg',
+                                 'annotation': 'png'
                                  },
-                     'cvat_image_1_1': {'image': 'jpg',
-                                        'annotation': 'xml'
-                                        },
-                     'cityscapes_val': {'image': 'png',
-                                        'annotation': 'json'
-                                        },
-                     'COCO2017': {'image': 'jpg',
-                                  'annotation': 'json'
-                                  },
-                     'cityscapes': {'image': 'png',
-                                    'annotation': 'json'
-                                    },
-                     'hy_val': {'image': 'png',
-                                'annotation': 'json'
-                                },
-                     'huaweiyun_segment': {'image': 'jpg',
-                                           'annotation': 'json'
-                                           },
-                     'hy_highway': {'image': 'png',
-                                    'annotation': 'xml'
-                                    },
-                     'pascal_voc': {'image': 'jpg',
-                                    'annotation': 'xml'
-                                    },
-                     'sjt': {'image': 'jpg',
-                             'annotation': 'json'
-                             },
-                     'tt100k': {'image': 'jpg',
-                                'annotation': 'json'
-                                },
-                     'yolo': {'image': 'jpg',
-                              'annotation': 'txt'
-                              },
-                     'yunce_segment_coco': {'image': 'jpg',
-                                            'annotation': 'json'
-                                            },
-                     'yunce_segment_coco_one_image': {'image': 'jpg',
-                                                      'annotation': 'json'
-                                                      },
-                     }
+    'bdd100k': {'image': 'jpg',
+                'annotation': 'json'
+                },
+    'cvat_image_1_1': {'image': 'jpg',
+                       'annotation': 'xml'
+                       },
+    'cityscapes_val': {'image': 'png',
+                       'annotation': 'json'
+                       },
+    'COCO2017': {'image': 'jpg',
+                 'annotation': 'json'
+                 },
+    'cityscapes': {'image': 'png',
+                   'annotation': 'json'
+                   },
+    'hy_val': {'image': 'png',
+               'annotation': 'json'
+               },
+    'huaweiyun_segment': {'image': 'jpg',
+                          'annotation': 'json'
+                          },
+    'hy_highway': {'image': 'png',
+                   'annotation': 'xml'
+                   },
+    'pascal_voc': {'image': 'jpg',
+                   'annotation': 'xml'
+                   },
+    'sjt': {'image': 'jpg',
+            'annotation': 'json'
+            },
+    'tt100k': {'image': 'jpg',
+               'annotation': 'json'
+               },
+    'yolo': {'image': 'jpg',
+             'annotation': 'txt'
+             },
+    'yunce_segment_coco': {'image': 'jpg',
+                           'annotation': 'json'
+                           },
+    'yunce_segment_coco_one_image': {'image': 'jpg',
+                                     'annotation': 'json'
+                                     },
+}
