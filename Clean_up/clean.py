dde28e55a32a41b000b50294d708d999f6f9bec5
leidi1989 <331926283@qq.com>
2022-02-06 00:49:37 +0800

    PDATE

--- Clean_up/clean.py
+++ Clean_up/clean.py
@@ -4,7 +4,7 @@ Version:
 Author: Leidi
 Date: 2021-08-04 16:45:50
 LastEditors: Leidi
-LastEditTime: 2022-02-06 00:25:27
+LastEditTime: 2022-02-06 00:46:41
 '''
 import time
 import yaml
@@ -27,18 +27,18 @@ def main(dataset_config: dict) -> None:
     #     print('Dataset initialize wrong, abort.')
         
     Input_dataset.source_dataset_copy_image_and_annotation()
-    # Input_dataset.transform_to_temp_dataset()
-    # Input_dataset.output_classname_file()
-    # Input_dataset.delete_redundant_image()
-    # Input_dataset.get_dataset_information()
+    Input_dataset.transform_to_temp_dataset()
+    Input_dataset.output_classname_file()
+    Input_dataset.delete_redundant_image()
+    Input_dataset.get_dataset_information()
 
-    # dataset.__dict__[dataset_config['Target_dataset_style']
-    #                  ].target_dataset(Input_dataset)
+    dataset.__dict__[dataset_config['Target_dataset_style']
+                     ].target_dataset(Input_dataset)
 
-    # Input_dataset.target_dataset_annotation_check()
+    Input_dataset.target_dataset_annotation_check()
 
-    # dataset.__dict__[dataset_config['Target_dataset_style']
-    #                  ].target_dataset_folder(Input_dataset)
+    dataset.__dict__[dataset_config['Target_dataset_style']
+                     ].target_dataset_folder(Input_dataset)
 
     return
 
