5a64586ca09d02f368791389d15fa77315286018
leidi1989 <331926283@qq.com>
2022-02-21 13:34:42 +0800

    update

--- Clean_up/clean.py
+++ Clean_up/clean.py
@@ -4,7 +4,7 @@ Version:
 Author: Leidi
 Date: 2021-08-04 16:45:50
 LastEditors: Leidi
-LastEditTime: 2022-02-21 13:24:06
+LastEditTime: 2022-02-21 13:29:43
 '''
 import time
 import yaml
@@ -27,11 +27,11 @@ def main(dataset_config: dict) -> None:
     #     print('Dataset initialize wrong, abort.')
         
     # Input_dataset.source_dataset_copy_image_and_annotation()
-    # Input_dataset.transform_to_temp_dataset()
-    # Input_dataset.output_classname_file()
-    # Input_dataset.delete_redundant_image()
+    Input_dataset.transform_to_temp_dataset()
+    Input_dataset.output_classname_file()
+    Input_dataset.delete_redundant_image()
     # Input_dataset.get_dataset_image_mean_std()
-    # Input_dataset.divide_dataset()
+    Input_dataset.divide_dataset()
     Input_dataset.sample_statistics()
     
     # 输出并检测指定形式数据集
